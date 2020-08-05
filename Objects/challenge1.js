0cbfbfacc4cfa8d110305418b53d33803430ee1f
Brandon Tabaska <btabaska@gmail.com>
2020-08-05 14:37:30 -0700

    Added runtask

--- Objects/challenge1.js
+++ Objects/challenge1.js
@@ -9,3 +9,5 @@ var confidential = {murder_weapon : "?", murderer : "?", murder_room : "?"}
 confidential.murder_room = rooms.pop(Math.random(0, 4))
 confidential.murder_weapon = weapons.pop(3)
 confidential.murderer = characters.pop(0)
+
+console.log(confidential)
